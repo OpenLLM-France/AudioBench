36aa5cf1da8bccb71c931451bb198298fe91b263
hedi naouara <hnaouara@gmail.com>
2026-02-26 12:30:27 +0000

    feat: add Linagora OSS-120B LLM judge eval method

--- src/dataset_src/base_dataset.py
+++ src/dataset_src/base_dataset.py
@@ -173,6 +173,15 @@ class BaseDatasetProcessor:
                 results, all_details = flow_judge_as_judge("", [questions, references, predictions])
             return {'flow_judge': results, 'details': all_details}
 
+        elif metrics == 'linagora_api_oss120':
+            if self.judge_binary:
+                from dataset_src.eval_methods.eval_linagora_api_oss120 import gpt4o_as_judge_binary
+                results, all_details = gpt4o_as_judge_binary("", [questions, references, predictions])
+            else:
+                from dataset_src.eval_methods.eval_linagora_api_oss120 import gpt4o_as_judge
+                results, all_details = gpt4o_as_judge("", [questions, references, predictions])
+            return {'linagora_api_oss120': results, 'details': all_details}
+
         elif metrics == 'meteor':
             import evaluate
             meteor = evaluate.load('meteor')
